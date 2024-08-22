2c9991c372f90ddf3d4814997c4462fd8ef1dd01
Your Name <you@example.com>
2024-08-22 23:18:38 +0530

    eks cluster updated

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
-
 terraform {
   required_version = ">= 0.12"
-required_providers {
+
+  required_providers {
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
+      version = ">= 2.7.1"
     }
     aws = {
       source  = "hashicorp/aws"
